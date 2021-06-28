8003a1c54d84250959df187db0d514c863fea19f
Pemma Reiter <pdreiter@asu.edu>
2021-06-28 16:52:34 -0700

    test file

---
+++ test_files/tmp_test1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+float abc(int x){
+ int y=10;
+ {
+    int z=10;
+ }
+ for (int a=0;a<x;a++){
+    int b=y-a;
+ }
+ unsigned char x[30]="ABC";
+}
+
+
+float HELPME(int x,int y, float a){
+
+   float i = 1.0;
+   int abc[35];
+   int job;
+   int b1,b2;
+   int a1=0,a2=1,a4;
+   int j=256;
+
+   float result=0;
+   result=100;
+   float f= (float)i;
+   const char x1[]="HELLOWORLD";
+   x=55;
+   for (unsigned char i =0; i<=sizeof(x1); i++){
+      x=y+a;
+
+      for (int jj,k=4,j=0; j<=k; j++){
+         char x[20]="Hello";
+      }
+      for (; i<=k; k--){
+      j=(++k)--;
+      result=sizeof(j);
+      }
+   }
+   x=66;
+
+   {
+      char cc;
+      cc=80;
+      result = result + f*a / (float)x;
+      job=x;
+      
+   }
+
+   char * x[3] = { "a", "b", "c" };
+
+   return result;
+
+}
+

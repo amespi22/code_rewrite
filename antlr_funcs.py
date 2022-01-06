857c7725d50870cd204c64cd73df2d4063ba85e2
Tony <amespi22@asu.edu>
2022-01-06 16:26:00 -0700

    fixed issue with no func args

--- antlr_funcs.py
+++ antlr_funcs.py
@@ -1871,7 +1871,7 @@ def get_func_args(ctx):
         else:
                 #print("error")
                 #print(ctx.getText())
-                return None
+                return []
     except Exception as e:
         print("Threw exception in get_func_args of antlr_funcs.py")
         print(e)
